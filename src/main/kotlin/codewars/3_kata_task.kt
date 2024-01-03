a5e65d990d2f9dc5fa0e7397fcefb81155d67caa
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-03 10:54:21 -0300

    2_sum_mixed_array

--- src/main/kotlin/codewars/3_kata_task.kt
+++ src/main/kotlin/codewars/3_kata_task.kt
@@ -20,3 +20,29 @@ package codewars
  * +9 dog years for second year
  * +5 dog years for each year after that
  */
+fun calculateYears(years: Int): Array<Int> {
+    var dogAge:Int = 0
+    var catAge: Int = 0
+
+    when (years) {
+        1 -> {
+            dogAge = 15
+            catAge = 15
+        }
+        2 -> {
+            dogAge = 24
+            catAge = 24
+        }
+        else -> {
+            dogAge = (years-2)*5 +24
+            catAge = (years-2)*4+ 24
+        }
+    }
+    return arrayOf(years, catAge, dogAge);
+}
+
+fun main(){
+    val years = 10
+    val result= calculateYears(years)
+    println("(${result.joinToString()})")
+}
