eae253f7faa48b56ef227b501c43d6ea80738507
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-04 14:31:20 -0300

    2_sum_mixed_array

---
+++ src/main/kotlin/codewars/6_ make_upper_case.kt
@@ -0,0 +1,11 @@
+package codewars
+
+/**
+Write a function which converts the input string to uppercase.
+ */
+fun makeUpperCase(str: String): String {
+    return str.uppercase()
+}
+fun main(){
+    println(makeUpperCase("paly"))
+}
