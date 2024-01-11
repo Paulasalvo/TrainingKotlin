de5b7f73dfd213babd1cf52e1c6b024454f358c9
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-11 11:58:30 -0300

    fun count and sum

---
+++ src/main/kotlin/codewars/15_convert_a_number_to_a_string!.kt
@@ -0,0 +1,8 @@
+package codewars
+
+fun numberToString(num: Int): String = num.toString()
+
+
+fun main(){
+    println(numberToString(6) is String)
+}
---
+++ src/main/kotlin/codewars/16_count_of_positives_sum_of_negatives.kt
@@ -0,0 +1,4 @@
+package codewars
+
+class `16_count_of_positives_sum_of_negatives` {
+}
