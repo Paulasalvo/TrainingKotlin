8e8b7dba7a85f9509bc403b5223cfec67b775812
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-15 00:43:06 -0300

    fun length shortest word

---
+++ src/main/kotlin/codewars/19_shortest_word.kt
@@ -0,0 +1,14 @@
+package codewars
+/**
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+*/
+
+fun findShort(s: String): Int {
+    return s.split(" ").sorted().first().length
+}
+
+
+fun main (){
+
+}
