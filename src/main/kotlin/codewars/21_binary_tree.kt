5c595c06a1d3b5caf0a950a030d471d39d88d0ad
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-17 01:01:40 -0300

    tree structure

---
+++ src/main/kotlin/codewars/21_binary_tree.kt
@@ -0,0 +1,31 @@
+
+/**
+ *
+ *               (1)
+ *             /     \
+ *          (2)     (3)
+ *        /    \
+ *      (4)   (5)
+ * */
+class NodeTree(
+    val value: Int,
+    var left: NodeTree? = null,
+    var right: NodeTree? = null
+)
+
+/**
+ *
+ *               (1)
+ *             /     \
+ *          (2)     (3)
+ *         /  \      /  \
+ *      null null  null null
+ *
+ * */
+fun main() {
+    val head = NodeTree(1)
+    head.left = NodeTree(2)
+    head.right = NodeTree(3)
+    head.left?.left = NodeTree(4)
+    head.left?.right = NodeTree(5)
+}
