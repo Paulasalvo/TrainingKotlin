d1e8161460a90173e91e071a758052747af4aea1
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-22 20:07:27 -0300

    fun using maxby and minby

--- src/main/kotlin/datastructure/3_hash_map.kt
+++ src/main/kotlin/datastructure/3_hash_map.kt
@@ -1,4 +1,15 @@
 package datastructure
 
-class `3_hash_map` {
+fun playgroundHashMap(){
+    val myMap: MutableMap<String, Int> = mutableMapOf()
+    myMap["enero"]=20
+    myMap["febrero"]=30
+    myMap["marzo"]=40
+    if (myMap.containsKey("marzo")){
+        println(myMap["marzo"])
+    }
+}
+
+fun main(){
+    playgroundHashMap()
 }
